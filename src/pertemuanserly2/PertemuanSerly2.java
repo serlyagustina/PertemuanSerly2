dae44b264e7331ec8bb02d287a6a8156f49b8a32
Serlyagustina <agustinaserly@gmail.com>
2024-10-08 08:18:09 +0100

    upload Form Hitung

---
+++ src/pertemuanserly2/PertemuanSerly2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuanserly2;
+
+/**
+ *
+ * @author SHERLY
+ */
+public class PertemuanSerly2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new fHitung().setVisible(true);
+    }
+    
+}
